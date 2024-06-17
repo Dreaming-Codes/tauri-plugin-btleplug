203e3fd1b71ef5b2e37aee6198f1873382911bcd
DreamingCodes <me@dreaming.codes>
2024-06-18 01:11:32 +0200

    Refactored java runtime code

--- src/java/btleplug_plugin.rs
+++ src/java/btleplug_plugin.rs
@@ -2,7 +2,7 @@
 
 use jni::JNIEnv;
 use jni::objects::{JClass};
-use crate::java::utils::{create_runtime, JAVAVM, setup_class_loader};
+use crate::java::utils::{setup_runtime, JAVAVM, setup_class_loader};
 
 #[no_mangle]
 pub extern "system" fn Java_com_plugin_btleplug_BtleplugPlugin_init(
@@ -11,11 +11,11 @@ pub extern "system" fn Java_com_plugin_btleplug_BtleplugPlugin_init(
 ) {
     btleplug::platform::init(&env).expect("Failed to initialize btleplug platform");
 
-    setup_class_loader(&env);
+    setup_class_loader(&env).expect("Failed to set class loader");
 
     if let Err(_) = JAVAVM.set(env.get_java_vm().expect("Failed to get JavaVM")) {
         panic!("Failed to set JavaVM for btleplug plugin");
     }
 
-    create_runtime();
+    setup_runtime().expect("Failed to setup tokio runtime");
 }
--- src/java/utils.rs
+++ src/java/utils.rs
@@ -1,71 +1,89 @@
 #![cfg(target_os = "android")]
 
 use std::cell::RefCell;
-use jni::{AttachGuard, JNIEnv};
-use jni::objects::GlobalRef;
-use tokio::sync::OnceCell;
 use std::sync::atomic::{AtomicUsize, Ordering};
+
+use jni::objects::GlobalRef;
+use jni::{AttachGuard, JNIEnv};
+use tokio::sync::OnceCell;
 
 static CLASS_LOADER: OnceCell<GlobalRef> = OnceCell::const_new();
 pub(crate) static JAVAVM: OnceCell<jni::JavaVM> = OnceCell::const_new();
 pub(crate) static RUNTIME: OnceCell<tokio::runtime::Runtime> = OnceCell::const_new();
 
-pub(crate) fn setup_class_loader(env: &JNIEnv) {
-    let thread = env
+pub(crate) fn get_java_thread<'a>(
+    jni_env: &'a JNIEnv<'a>,
+) -> Result<jni::objects::JObject<'a>, jni::errors::Error> {
+    Ok(jni_env
         .call_static_method(
             "java/lang/Thread",
             "currentThread",
             "()Ljava/lang/Thread;",
             &[],
-        )
-        .expect("Failed to get current java thread")
-        .l()
-        .expect("Failed to get current java thread object");
+        )?
+        .l()?)
+}
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum SetupClassloaderError {
+    #[error(transparent)]
+    Jni(#[from] jni::errors::Error),
+    #[error("Failed to set class loader")]
+    ClassLoaderAlreadySet,
+}
+
+pub(crate) fn setup_class_loader(env: &JNIEnv) -> Result<(), SetupClassloaderError> {
+    let thread = get_java_thread(env).expect("Failed to get current java thread");
     let class_loader = env
         .call_method(
             thread,
             "getContextClassLoader",
             "()Ljava/lang/ClassLoader;",
             &[],
-        )
-        .expect("Failed to get class loader")
-        .l()
-        .expect("Failed to get class loader object");
+        )?
+        .l()?;
 
-    let _ = CLASS_LOADER.set(env.new_global_ref(class_loader).expect("Failed to create global ref"));
+    CLASS_LOADER.set(env.new_global_ref(class_loader)?).map_err(|_| SetupClassloaderError::ClassLoaderAlreadySet)?;
+
+    Ok(())
 }
 
-pub(crate) fn load_btleplug_context() {
-    let vm = JAVAVM.get().expect("JavaVM not set");
-    let env = vm.attach_current_thread().expect("Failed to attach to JVM");
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum LoadBtleplugContextError {
+    #[error(transparent)]
+    Jni(#[from] jni::errors::Error),
+    #[error("Failed to get current java thread")]
+    FailedToGetJavaThread,
+}
 
-    let thread = env
-        .call_static_method(
-            "java/lang/Thread",
-            "currentThread",
-            "()Ljava/lang/Thread;",
-            &[],
-        )
-        .expect("Failed to get current java thread")
-        .l()
-        .expect("Failed to get current java thread object");
+pub(crate) fn load_btleplug_context() -> Result<(), LoadBtleplugContextError> {
+    let vm = JAVAVM.get().ok_or(LoadBtleplugContextError::FailedToGetJavaThread)?;
+    let env = vm.attach_current_thread()?;
+
+    let thread = get_java_thread(&env)?;
     env.call_method(
         thread,
         "setContextClassLoader",
         "(Ljava/lang/ClassLoader;)V",
         &[CLASS_LOADER.get().unwrap().as_obj().into()],
-    )
-        .expect("Failed to set class loader context");
+    )?;
+
+    Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum SetupRuntimeError {
+    #[error("Failed to create jni attached runtime")]
+    RuntimeCreation(#[source] std::io::Error),
+    #[error("Failed to set runtime")]
+    RuntimeAlreadySet,
 }
 
 std::thread_local! {
     static JNI_ENV: RefCell<Option<AttachGuard<'static>>> = RefCell::new(None);
 }
 
-pub(crate) fn create_runtime() {
-    let vm = JAVAVM.get().expect("JavaVM not set");
-    let env = vm.attach_current_thread().expect("Failed to attach to JVM");
-
+pub(crate) fn setup_runtime() -> Result<(), SetupRuntimeError>{
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .thread_name_fn(|| {
@@ -80,12 +98,14 @@ pub(crate) fn create_runtime() {
             let vm = JAVAVM.get().expect("JavaVM not set");
             let env = vm.attach_current_thread().expect("Failed to attach to JVM");
 
-            load_btleplug_context();
+            load_btleplug_context().expect("Failed to load btleplug context");
 
             JNI_ENV.with(|f| *f.borrow_mut() = Some(env));
         })
         .build()
-        .expect("Failed to create runtime");
-    
-    RUNTIME.set(runtime).expect("Failed to set runtime");
+        .map_err(SetupRuntimeError::RuntimeCreation)?;
+
+    RUNTIME.set(runtime).map_err(|_| SetupRuntimeError::RuntimeAlreadySet)?;
+
+    Ok(())
 }
